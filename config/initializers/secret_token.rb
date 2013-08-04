e6c1b025e35a2b000d5ac924c8a6b23eccc6c1c9
Slawomir Demichowicz <slawomir.demichowicz@teamaol.com>
2013-08-04 23:37:53 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Battleships::Application.config.secret_key_base = '063d23f6b5b16fa9ae0c4290995833bc8375407aaa528d62d54ea7ce1815f10342a85608feffcae64be6c8bbbd29d2fdb47e70c0b378b91816124ecb161cdb0e'
