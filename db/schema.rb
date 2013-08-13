4b0c28dc083d36bae949d27748d204ef9bc5ee07
Alvaro Pereda <alvaro.pereda.sancho@teamaol.com>
2013-08-13 22:52:24 +0100

    Schema updated.

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130813191852) do
-
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
