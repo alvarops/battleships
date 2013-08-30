0e6ff02d5bc8bdfe9118df4b360f50faf7e4bcb3
Alvaro Pereda <alvaro.pereda.sancho@teamaol.com>
2013-08-30 18:31:05 +0100

    forcing reload

--- test/models/shoot_test.rb
+++ test/models/shoot_test.rb
@@ -4,4 +4,9 @@ class ShootTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'should shoot and miss' do
+    Shoot.create do |s|
+      s.
+    end
+  end
 end
