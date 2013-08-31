9e7e9b0a8399a1cd9922fe30e7082007e27ccac0
Alvaro Pereda <alvaro.pereda.sancho@teamaol.com>
2013-08-31 21:33:52 +0100

    Check board_id when creating.

--- test/integration/new_shoot_test.rb
+++ test/integration/new_shoot_test.rb
@@ -2,10 +2,10 @@ require 'test_helper'
 
 class NewShootTest < ActionDispatch::IntegrationTest
   test 'should shoot started games' do
-    get '23j0f023912309r5u11fas/game/1/shoot', {:x => 5, :y => 5}
+    get '23j0f023912309r5u11fas/game/2/shoot', {:x => 5, :y => 5}
 
     shoot = JSON.parse @response.body
-
     assert_not_nil shoot
+    assert_equal shoot['board_id'], 10002
   end
 end
