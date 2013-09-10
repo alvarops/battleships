f9f7e1a7620671bd317fe13de52fd0d7f3455d6b
Slawomir Demichowicz <slawomir.demichowicz@teamaol.com>
2013-09-10 21:50:59 +0100

    Fixed tst

--- test/integration/show_board_test.rb
+++ test/integration/show_board_test.rb
@@ -4,6 +4,7 @@ class ShowBoardTest < ActionDispatch::IntegrationTest
 
   test 'GET show board' do
     get '23j0f023912309r5u11fas/game/2/randomize'
+
     assert_equal 200, @response.status
 
     get '23j0f023912309r5u11fas/game/2/show'
@@ -12,18 +13,15 @@ class ShowBoardTest < ActionDispatch::IntegrationTest
 
     game = Game.find 2
 
-    b = Board.find 10002
-    assert_equal ShipShapes::SHIP_TYPES.length ,b.ships.length, 'wrong number of ships on board'
+    b = Board.find 10001
     resp = @response.body
-
-    assert_equal 200, @response.status
-
     numberOfPixelsOfShipsForAPlayer = 17
 
+    assert_equal ShipShapes::SHIP_TYPES.length ,b.ships.length, 'wrong number of ships on board'
+    assert_equal 200, @response.status
     assert resp.match(/#{game.width-1}x#{game.height-1}/), 'board size unknown'
     assert resp.match /data-width='#{game.width}'/
     assert resp.match /data-height='#{game.height}'/
-
     assert_equal numberOfPixelsOfShipsForAPlayer, (resp.scan '<td class="red">').size, 'Numbers of pixels does not match'
 
   end
