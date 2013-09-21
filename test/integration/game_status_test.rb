49fd61db80688849d9c96476068de51860a4f1f5
Slawomir Demichowicz <slawomir.demichowicz@teamaol.com>
2013-09-21 01:25:57 +0100

    Added more assertion to e2e test.

--- test/integration/game_status_test.rb
+++ test/integration/game_status_test.rb
@@ -2,71 +2,119 @@ require 'test_helper'
 
 class GameStatusTest < ActionDispatch::IntegrationTest
 
+
   test 'full end to end test' do
-    # CREATE PLAYER 1
-    get '/player/new?name=P1'
-    resp = JSON.parse @response.body
-    assert_equal 201, @response.status
-    assert_equal 'P1', resp['name']
-    token_1 = resp['token']
-    id_1 = resp['id']
+    player1, player2 = create_and_verify_players()
+    game = create_and_verify_game(player1)
+    join_game(game, player2)
 
-    # CREATE PLAYER 2
-    get '/player/new?name=P2'
-    resp = JSON.parse @response.body
-    assert_equal 201, @response.status
-    assert_equal 'P2', resp['name']
-    id_2 = resp['id']
-    token_2 = resp['token']
+    request_and_verify_game_status(game, player2, 'ready')
 
-    #CREATE A NEW GAME
-    get '/' + token_1 + '/game/new'
+    randomize_ships(game, player1)
+    request_and_verify_game_status(game, player1, 'ready')
+
+    randomize_ships(game, player2)
+    request_and_verify_game_status(game, player2, 'fight')
+  end
+
+  private
+
+  def resp_body
+    JSON.parse @response.body
+  end
+
+  def resp
+    @response
+  end
+
+  def create_and_verify_game(player)
+    get "/#{player['token']}/game/new"
+    game = resp_body
+    verify_game(game, 'created', 1)
+    game
+  end
+
+  def request_and_verify_game_status(game, player, game_status)
+    get "/#{player['token']}/game/#{game['id']}"
+    assert_equal 200, resp.status
+    verify_game_status resp_body, game_status
+  end
+
+
+  def join_game(game, player)
+    get "/#{player['token']}/game/#{game['id']}/join"
+    assert_equal 200, resp.status
+  end
+
+  def create_and_verify_players
+    player1 = create_player 'P1'
+    verify_player player1, 'P1'
+
+    player2 = create_player 'P2'
+    verify_player player2, 'P2'
+    return player1, player2
+  end
+
+  def create_player(name)
+    get "/player/new?name=#{name}"
+    resp_body
+  end
+
+  def randomize_ships(game, player)
+    get "/#{player['token']}/game/#{game['id']}/randomize"
     assert_equal 200, @response.status
-    resp = JSON.parse @response.body
-    game_id = resp['id']
-    assert_equal 'created', resp['status'], ' Incorrect game status'
+  end
 
-    # JOIN SECOND PLAYER
-    get '/' + token_2.to_s + '/game/' + game_id.to_s + '/join'
-    assert_equal 200, @response.status
-    resp = JSON.parse @response.body
+  def verify_player(player, name)
+    assert_equal 201,  resp.status
+    assert_equal name, player['name']
+    assert_equal 22,   player['token'].length
+    assert_equal 0,    player['won']
+    assert_equal 0,    player['lost']
+    assert_not_nil     player['id']
+  end
 
-    #CHECK GAME STATUS
-    get '/' + token_2.to_s + '/game/' + game_id.to_s
-    assert_equal 200, @response.status
-    resp = JSON.parse @response.body
-    assert_equal 'ready', resp['status'], ' Incorrect game status'
+  def verify_game(game, status, no_of_players)
+    assert_equal 200, resp.status
+    assert_equal status, game['status'], 'Incorrect game status'
+    assert game['width']  > 0
+    assert game['height'] > 0
+    assert game['id']     > 0
+    assert_equal no_of_players, game['players'].length
+  end
 
-    #PLACE SHIPS ON PLAYER 1 BOARD
-    get '/' + token_1.to_s + '/game/' + game_id.to_s + '/randomize'
-    assert_equal 200, @response.status
-    resp = JSON.parse @response.body
+  def verify_game_status(game, status)
+    assert_not_nil game['id']
+    assert_not_nil game['width']
+    assert_not_nil game['height']
 
-    #CHECK GAME STATUS AS PLAYER 1
-    get '/' + token_1.to_s + '/game/' + game_id.to_s
-    assert_equal 200, @response.status
-    resp = JSON.parse @response.body
-    assert_equal 'ready', resp['status'], ' Incorrect game status'
+    assert_equal status, game['status'], 'Incorrect game status'
 
-    #CHECK GAME STATUS AS PLAYER 2
-    get '/' + token_2.to_s + '/game/' + game_id.to_s
-    assert_equal 200, @response.status
-    resp = JSON.parse @response.body
-    assert_equal 'ready', resp['status'], ' Incorrect game status'
+    players = game['players']
 
-    #PLACE SHIPS ON PLAYER 2 BOARD
-    get '/' + token_2.to_s + '/game/' + game_id.to_s + '/randomize'
-    assert_equal 200, @response.status
-    resp = JSON.parse @response.body
+    assert_equal 2,    players.length
+    verify_game_player players[0], 'P1'
+    verify_game_player players[1], 'P2'
 
-    #CHECK GAME STATUS
-    get '/' + token_2.to_s + '/game/' + game_id.to_s
-    assert_equal 200, @response.status
-    resp = JSON.parse @response.body
-    assert_equal 'fight', resp['status'], ' Incorrect game status'
+    boards = game['boards']
 
-    puts resp
+    assert_equal 2, boards.length
+    verify_board boards[0], players[0]['id'], game['id']
+    verify_board boards[1], players[1]['id'], game['id']
 
   end
 
+  def verify_game_player(player, name)
+    assert_equal name, player['name']
+    assert_nil         player['token'] #token cannot be exposed in the game satus
+    assert_not_nil     player['id']
+    assert_equal 0,    player['won']
+    assert_equal 0,    player['lost']
+  end
+
+  def verify_board(board, player_id, game_id)
+    assert_not_nil board['id']
+    assert_equal player_id, board['player_id']
+    assert_equal game_id,   board['game_id']
+  end
 end
